f1f0ec20b2fc266275bd9adce291e356a2f0a9cd
Kaur-code <79134890+Kaur-code@users.noreply.github.com>
2021-04-13 16:09:47 +0300

    Update projektikolme.js

---
+++ Projekti3/projektikolme.js
@@ -0,0 +1,28 @@
+var tappiot = 0;
+var voitot = 0;
+
+var kerrat3 = 0;
+function pelaa() {
+  let rantom = Math.floor(Math.random()*10)+1;
+  console.log(rantom)
+  kerrat3++
+  for(let i = 0; i < 10; i++) {
+      if(i == numeroSyotto.value){
+        if(kerrat3 >= 4) {
+          alert("Käytit kaikki kerrat!");
+        } else if(numeroSyotto.value > rantom) {
+          alert("Valitsit liian SUUREN numeron!");
+          tappiot++;
+          return document.getElementById("tappiot").innerHTML = "Tappiot: " + tappiot;
+        } else if(numeroSyotto.value < rantom) {
+          alert("Valitsit liian pienen numeron!");
+          tappiot++;
+          return document.getElementById("tappiot").innerHTML = "Tappiot: " + tappiot;
+        } else if(numeroSyotto.value == rantom) {
+          alert("Arvasit oikein!");
+          voitot++;
+          return document.getElementById("voitot").innerHTML = "Voitot: " + voitot;
+        }
+      }
+    }
+  }
