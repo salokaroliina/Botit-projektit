d2b6d986ba14f6b85a1983f3cc4b1bdc4f01bb8c
unnap <59951708+unnap@users.noreply.github.com>
2021-04-29 10:52:41 +0300

    eka versio javascript & placeholder kortit

---
+++ muistipeli/muistipeli.js
@@ -0,0 +1,126 @@
+const kortit = [
+              {pari: 'A', kuva: 'img/ph1.jpg'},
+              {pari: 'A', kuva: 'img/ph1.jpg'},
+              {pari: 'B', kuva: 'img/ph2.jpg'},
+              {pari: 'B', kuva: 'img/ph2.jpg'},
+              {pari: 'C', kuva: 'img/ph3.jpg'},
+              {pari: 'C', kuva: 'img/ph3.jpg'},
+              {pari: 'D', kuva: 'img/ph4.jpg'},
+              {pari: 'D', kuva: 'img/ph4.jpg'},
+              {pari: 'E', kuva: 'img/ph5.jpg'},
+              {pari: 'E', kuva: 'img/ph5.jpg'},
+              {pari: 'F', kuva: 'img/ph6.jpg'},
+              {pari: 'F', kuva: 'img/ph6.jpg'},
+              {pari: 'G', kuva: 'img/ph7.jpg'},
+              {pari: 'G', kuva: 'img/ph7.jpg'},
+              {pari: 'H', kuva: 'img/ph8.jpg'},
+              {pari: 'H', kuva: 'img/ph8.jpg'},
+              {pari: 'I', kuva: 'img/ph9.jpg'},
+              {pari: 'I', kuva: 'img/ph9.jpg'},
+              {pari: 'J', kuva: 'img/ph10.jpg'},
+              {pari: 'J', kuva: 'img/ph10.jpg'},
+              {pari: 'K', kuva: 'img/ph11.jpg'},
+              {pari: 'K', kuva: 'img/ph11.jpg'},
+              {pari: 'L', kuva: 'img/ph12.jpg'},
+              {pari: 'L', kuva: 'img/ph12.jpg'},
+              {pari: 'M', kuva: 'img/ph13.jpg'},
+              {pari: 'M', kuva: 'img/ph13.jpg'},
+              {pari: 'N', kuva: 'img/ph14.jpg'},
+              {pari: 'N', kuva: 'img/ph14.jpg'},
+              {pari: 'O', kuva: 'img/ph15.jpg'},
+              {pari: 'O', kuva: 'img/ph15.jpg'},
+              {pari: 'P', kuva: 'img/ph16.jpg'},
+              {pari: 'P', kuva: 'img/ph16.jpg'},
+              {pari: 'Q', kuva: 'img/ph17.jpg'},
+              {pari: 'Q', kuva: 'img/ph17.jpg'},
+              {pari: 'R', kuva: 'img/ph18.jpg'},
+              {pari: 'R', kuva: 'img/ph18.jpg'}
+            ];
+let pakka = [];
+let valitut = [];
+let parit = [];
+
+function luoPakka4x4() {
+  for(let i=0;i<16;i++) {
+    pakka.push(kortit[i]);
+  }
+  let temp;
+  for(let i=pakka.length-1;i>0;i--) {
+    let rando = Math.floor(Math.random()*(i+1));
+    temp = pakka[i];
+    pakka[i] = pakka[rando];
+    pakka[rando] = temp;
+  }
+}
+
+function tyhjenna() {
+  pakka = [];
+  console.log(pakka);
+}
+
+function luoLauta() {
+  for(let i=0;i<16;i++) {
+    let lauta = document.getElementById('eka');
+    //let kortti = document.createElement('div');
+    let kortti = document.createElement('img');
+    kortti.setAttribute('src', 'img/card_back.jpg');
+    //kortti.appendChild(img);
+    kortti.classList.add('kortti');
+    kortti.dataset.pari = pakka[i].pari;
+    kortti.id = i;
+    kortti.addEventListener('click', kaanna);
+    lauta.appendChild(kortti);
+  }
+}
+
+function kaanna() {
+  let valittu = this.id;
+  valitut.push(this.dataset.pari);
+  valitut.push(this.id);
+  this.setAttribute('src', pakka[valittu].kuva);
+
+  document.getElementById(valittu).removeEventListener('click', kaanna)
+
+  if (valitut.length === 4) {
+    for(let i=0;i<pakka.length;i++) {
+      if (document.getElementById(i) !== null) {
+      document.getElementById(i).removeEventListener('click', kaanna);
+      }
+    }
+    setTimeout(function() {
+      if (valitut[0] === valitut[2]) {
+        alert("voitit töttöröö");
+        parit.push(valitut[1]);
+        parit.push(valitut[3]);
+        document.getElementById(valitut[1]).id += 'match';
+        document.getElementById(valitut[3]).id += 'match';
+        console.log(parit);
+        valitut = [];
+      } else {
+        alert("hävisit");
+        document.getElementById(valitut[3]).setAttribute('src', 'img/card_back.jpg');
+        document.getElementById(valitut[1]).setAttribute('src', 'img/card_back.jpg');
+
+        valitut = [];
+      }
+      console.log(pakka[valittu].kuva)
+      for(let i=0;i<pakka.length;i++) {
+        if (document.getElementById(i) !== null) {
+        document.getElementById(i).addEventListener('click', kaanna);
+        }
+      }
+    }, 700)
+  } else {
+
+  }
+}
+
+function testi() {
+  console.log(pakka);
+  document.getElementsByClassName('klikattu').innerHTML = 'tuut';
+  document.getElementById(21).id += 'string';
+}
+
+function vapautus() {
+
+}
