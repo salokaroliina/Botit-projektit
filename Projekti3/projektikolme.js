171f0cf292b43af216a54dfd3797ab87e35bd415
Karoliina Salo <salo.karoliina@gmail.com>
2021-05-08 15:48:40 +0300

    Korjattu javascript ja hiottu html / 8.5.2021 / Karoliina Salo

--- Projekti3/projektikolme.js
+++ Projekti3/projektikolme.js
@@ -1,34 +1,56 @@
 // Työnjako ja osallistujat:
 // Html: Karoliina Salo
-// Css: Niko Särkiranta
-// JS: Kaur Sarv
-// Niko Jokinen ei ollut paikalla eikä osallistunut
+// Css: Simo Särkiranta
+// JS: Karoliina Salo
 
-var tappiot = 0;
-var voitot = 0;
 
-var kerrat3 = 0;
-function pelaa() {
-  let rantom = Math.floor(Math.random()*10)+1;
-  console.log(rantom)
-  kerrat3++
-  for(let i = 0; i < 10; i++) {
+// luodaan muuttuja "satunnainen" funktion ulkopuolelle
+// muuttujan ollessa funktion ulkopuolella, funktio ei vaikuta siihen
+// ja satunnainen luku pysyy samana
+var satunnainen = Math.floor(Math.random()*10)+1;
+// muuttujat voittto, tappio ja kerrat ovat myös funktion ulkopuolella
+var voitto = 0;
+var tappio = 0;
+var kerrat = 0;
+// funktio
+function pelaa(){
+  // logista voi tarkistaa satunnaisluvun
+  console.log(satunnainen);
+  // aina funktion käynnistyessä (nappia painamalla), kerrat saa yhden lisää
+  // kertoja on enintään 3
+  kerrat++;
+  // for-loop
+  for(var i = 0; i < 11; i++) {
+    // verrataan i:tä käyttäjän syöttämään lukuun
       if(i == numeroSyotto.value){
-        if(kerrat3 >= 4) {
-          alert("Käytit kaikki kerrat!");
-        } else if(numeroSyotto.value > rantom) {
+        // kun käyttäjä painaa nappia neljännen kerran, ohjelma kertoo pelaajan käyttäneen kaikki kerrat
+        // peli voi päättyä myös käyttäjän arvattua oikein ja saatua 1 pisteen
+        if(kerrat >= 4 || voitto == 1) {
+          if (kerrat >= 4) {
+            alert("Käytit kaikki kerrat!");
+          }
+          else {
+            alert("Olet jo voittanut!");
+          }
+          // jos luku on suurempi kuin satunnainen luku, ohjelma kertoo numeron olevan liian suuri
+        } else if(numeroSyotto.value > satunnainen) {
           alert("Valitsit liian SUUREN numeron!");
-          tappiot++;
-          return document.getElementById("tappiot").innerHTML = "Tappiot: " + tappiot;
-        } else if(numeroSyotto.value < rantom) {
+          // numeron ollessa väärä, tappiot kasvavat yhdellä
+          tappio++;
+          return document.getElementById("tappiot").innerHTML = "Tappiot: " + tappio;
+        } // jos luku on pienempi kuin satunnainen luku, ohjelma kertoo numeron olevan liian pieni
+          else if(numeroSyotto.value < satunnainen) {
           alert("Valitsit liian pienen numeron!");
-          tappiot++;
-          return document.getElementById("tappiot").innerHTML = "Tappiot: " + tappiot;
-        } else if(numeroSyotto.value == rantom) {
-          alert("Arvasit oikein!");
-          voitot++;
-          return document.getElementById("voitot").innerHTML = "Voitot: " + voitot;
+          // numeron ollessa väärä, tappiot kasvavat yhdellä
+          tappio++;
+          return document.getElementById("tappiot").innerHTML = "Tappiot: " + tappio;
+        } // jos numero on sama kuin satunnainen luku, pelaaja voittaa
+          else if(numeroSyotto.value == satunnainen) {
+          alert("Arvasit oikein! Peli päättyy");
+          // pelaajan voittaessa voitto kasvaa yhdellä
+          voitto++;
+          return document.getElementById("voitot").innerHTML = "VOITTO!";
         }
       }
     }
-  }
+}
