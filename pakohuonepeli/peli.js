f493d0d4890c2ed78c8532f477ef07a4aee72a75
Karoliina Salo <salo.karoliina@gmail.com>
2021-05-27 10:55:24 +0300

    Lisätty js muuttujat / 27.5.2021 / Karoliina S.

---
+++ pakohuonepeli/peli.js
@@ -0,0 +1,24 @@
+// modal & sulje-näppäin
+const modal = document.getElementsByClassName('modal');
+const x = document.getElementsByClassName('close');
+// erilliset objektit
+const lockKey = document.getElementById('lock-key');
+const lockCode = document.getElementById('lock-code');
+const lockElec = document.getElementById('lock-elec');
+const box = document.getElementById('box');
+const key = document.getElementById('key');
+const note = document.getElementById('note');
+const doorOpen = document.getElementById('door-open');
+const pot = document.getElementById('pot');
+const curtain = document.getElementById('curtain');
+// taustaelementit
+const door = document.getElementById('hit-door');
+const frame = document.getElementById('hit-frame');
+const clock = document.getElementById('hit-clock');
+const monitor = document.getElementById('hit-monitor');
+const pc = document.getElementById('hit-pc');
+const wind = document.getElementById('hit-window');
+const bookOne = document.getElementById('hit-book1');
+const bookTwo = document.getElementById('hit-book2');
+const bookThree = document.getElementById('hit-book3');
+const bookFour = document.getElementById('hit-book4');
