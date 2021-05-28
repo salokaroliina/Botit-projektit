90760e97e41b0821ea74e49febf7251852fcb592
Karoliina Salo <salo.karoliina@gmail.com>
2021-05-28 20:08:33 +0300

    Lisätty scriptia / 28.5.2021 / Karoliina S.

--- pakohuonepeli/peli.js
+++ pakohuonepeli/peli.js
@@ -22,8 +22,8 @@ const bookOne = document.getElementById('hit-book1');
 const bookTwo = document.getElementById('hit-book2');
 const bookThree = document.getElementById('hit-book3');
 const bookFour = document.getElementById('hit-book4');
-
 const koodi = document.getElementById('lockbutton');
+
 
 function popup(x) {
   modal[x].style.display = 'block';
@@ -41,12 +41,28 @@ function shut(button) {
   button.parentNode.parentNode.style.display = 'none';
 }
 
+// koodilukko -K
 function codeLock()
 {
   if (koodi.value == '1234') {
     console.log("Oikein");
+    lockCode.remove();
   }
   else {
     console.log("Väärin");
   }
+}
+
+// klikkaus siirtää ruukkua -K
+// keksi vielä miten ruukun saa klikattua takaisin paikalleen -K
+function movePot(){
+  pot.style.left = '450px';
+}
+
+
+// klikkaus poistaa verhon -K
+// keksi miten verhon saa takaisin -K
+function curtainOff(){
+  curtain.remove();
+  modal[17].style.display = 'none';
 }
