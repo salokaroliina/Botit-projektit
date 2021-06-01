f2af534084f71111dd9f02136c7701893a653980
Karoliina Salo <salo.karoliina@gmail.com>
2021-06-01 18:18:11 +0300

    Lisätty kommentteja / 1.6.2021 / Karoliina S.

--- pakohuonepeli/peli.js
+++ pakohuonepeli/peli.js
@@ -1,10 +1,11 @@
-// modal & sulje-näppäin
+// modal & sulje-näppäin -K
 const modal = document.getElementsByClassName('modal');
 const x = document.getElementsByClassName('close');
-// erilliset objektit
+// erilliset objektit -K
 const lockKey = document.getElementById('lock-key');
 const lockCode = document.getElementById('lock-code');
 const lockElec = document.getElementById('lock-elec');
+const code = document.getElementById('lockbutton');
 const box = document.getElementById('box');
 const key = document.getElementById('key');
 const note = document.getElementById('note');
@@ -13,7 +14,7 @@ const curtain = document.getElementById('curtain');
 const bag = document.getElementById('bag');
 const jacket = document.getElementById('jacket');
 const pot = document.getElementsByClassName('pot')[0];
-// taustaelementit
+// taustaelementit -K
 const door = document.getElementById('hit-door');
 const frame = document.getElementById('hit-frame');
 const clock = document.getElementById('hit-clock');
@@ -24,10 +25,11 @@ const bookOne = document.getElementById('hit-book1');
 const bookTwo = document.getElementById('hit-book2');
 const bookThree = document.getElementById('hit-book3');
 const bookFour = document.getElementById('hit-book4');
-const koodi = document.getElementById('lockbutton');
-
+// inventaario on pelissä vain array -K
+// fyysiselle inventaariolle ei ollut tarvetta -K
 let inventory = [];
 
+// Unna teki nämä -->
 function popup(x) {
   modal[x].style.display = 'block';
 }
@@ -43,23 +45,33 @@ window.onclick = function(event) {
 function shut(button) {
   button.parentNode.parentNode.style.display = 'none';
 }
+// <--
 
 // koodilukko -K
-function codeLock()
-{  if (koodi.value == '1234') {
-    console.log("Oikein");
+function codeLock(){
+  // jos koodi on 6174, lukko poistuu ja modaaliin tulee teksti joka kertoo
+  // lukon olevan auki
+  if (code.value == '6174') {
+    //console.log("Oikein");
     lockCode.remove();
+    document.getElementById('locked').innerHTML = "The lock opened!";
   }
   else {
-    console.log("Väärin");
+    // jos koodi on väärin, modaaliin tulee teksti joka kertoo koodin olevan väärä
+    //console.log("Väärin");
+    document.getElementById('locked').innerHTML = "The code is wrong!";
   }
+    // 'klick' -näppäimen painalluksen jälkeen input-alue tyhjenee
+    document.getElementById('codeinput').reset();
 }
 
 // klikkaus siirtää laukun inventoryyn -K
 function takeBag(){
+  // laukku poistuu ruudulta
   bag.remove();
-inventory.push(bag);
-console.log(inventory);
+  // laukku siirtyy inventoryyn
+  inventory.push(bag);
+  //console.log(inventory);
 }
 
 // klikkaus poistaa verhon -K
@@ -69,12 +81,17 @@ function curtainOff(){
 
 // klikkaus siirtää takin inventoryyn -K
 function takeJacket(){
+  // takki poistuu ruudulta
   jacket.remove();
+  // takki siirtyy inventoryyn
   inventory.push(jacket);
-  console.log(inventory);
+  //console.log(inventory);
 }
 
 // klikkaus siirtää ruukkua edes takaisin -K
 function movePot(){
+  // ruukun(pot) sijainnit määritetään css:ssä:
+  // lähtötilanne omanaan, sekä uusi, .movePot omanaan.
+  // ruukku siirtyy klikkauksesta näiden kahden sijainnin välillä
   pot.classList.toggle('movePot');
 }
